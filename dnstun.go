88db3bbe412f68c888746daf5c09e45c5f34d672
Yasha Bubnov <ybubnov@gojuno.com>
2019-10-09 16:08:15 +0300

    Request remote resolver through gRPC.

--- dnstun.go
+++ dnstun.go
@@ -6,29 +6,72 @@ import (
 	"context"
 
 	"github.com/coredns/coredns/plugin"
+	"github.com/coredns/coredns/request"
 	"github.com/miekg/dns"
+	"google.golang.org/grpc"
 )
+
+type Options struct {
+	Host string
+}
 
 // DNSTun is a plugin to block DNS tunneling queries.
 type DNSTun struct {
-	Next plugin.Handler
-
-	Resolver ResolverClient
+	resolver ResolverClient
 }
 
-func NewDNSTun() *DNSTun {
-	return &DNSTun{
-		Next:     nil,
-		Resolver: NewResolverClient(nil),
+func NewDNSTun(opts Options) (*DNSTun, error) {
+	conn, err := grpc.Dial(opts.Host)
+	if err != nil {
+		return nil, err
 	}
+	return &DNSTun{
+		resolver: NewResolverClient(conn),
+	}, nil
+}
+
+func (dt *DNSTun) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+	state := request.Request{W: w, Req: r}
+	req := ResolveRequest{Name: state.QName()}
+
+	// When the remote resolver is not available, simply forward request
+	// processing to the next handler as there is no error.
+	resp, err := dt.resolver.Resolve(ctx, &req)
+	if err != nil {
+		return dns.RcodeSuccess, nil
+	}
+
+	switch resp.Action {
+	case Action_REJECT:
+		m := new(dns.Msg)
+		m.SetRcode(r, dns.RcodeRefused)
+		w.WriteMsg(m)
+
+		return dns.RcodeRefused, nil
+	}
+
+	// Pass the control to the next plugin.
+	return dns.RcodeSuccess, nil
+}
+
+type chainPlugin struct {
+	name     string
+	next     plugin.Handler
+	serveDNS func(context.Context, dns.ResponseWriter, *dns.Msg) (int, error)
 }
 
 // Name returns the name of the plugin. This method implements plugin.Handler
 // interface.
-func (dt *DNSTun) Name() string {
-	return "dnstun"
+func (p chainPlugin) Name() string {
+	return p.name
 }
 
-func (dt *DNSTun) ServeDNS(ctx context.Context, rw dns.ResponseWriter, r *dns.Msg) (int, error) {
-	return 0, nil
+func (p chainPlugin) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+	rcode, err := p.serveDNS(ctx, w, r)
+	if rcode != dns.RcodeSuccess {
+		return rcode, err
+	}
+
+	state := request.Request{W: w, Req: r}
+	return plugin.NextOrFailure(state.Name(), p.next, ctx, w, r)
 }
--- setup.go
+++ setup.go
@@ -4,15 +4,19 @@ import (
 	"github.com/caddyserver/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
+	"github.com/pkg/errors"
 )
 
 func init() { plugin.Register("dnstun", setup) }
 
 func setup(c *caddy.Controller) error {
+	p, err := NewDNSTun(Options{})
+	if err != nil {
+		return plugin.Error("dnstun", errors.Wrapf(err, "failed to create plugin"))
+	}
 
 	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
-		return NewDNSTun()
+		return chainPlugin{"dnstun", next, p.ServeDNS}
 	})
-
 	return nil
 }
